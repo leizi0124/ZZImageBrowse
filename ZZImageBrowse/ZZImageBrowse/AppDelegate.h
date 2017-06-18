fd10fb6c355b9b4a1c3d80b17223b8ab989733f9
leizi0124 <2444733544@qq.com>
2017-06-18 18:18:40 +0800

    创建工程
    创建图片浏览工程

---
+++ ZZImageBrowse/ZZImageBrowse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZImageBrowse
+//
+//  Created by 赵振磊 on 2017/6/18.
+//  Copyright © 2017年 赵振磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
